fbb593f69287eb5e7e69bcfc0520f4d0e0ebc626
egiw <egisolehhasdi@gmail.com>
2013-10-18 00:19:17 +0700

    testing

--- public/index.php
+++ public/index.php
@@ -1,63 +1,161 @@
 <?php
 
-/* @var $em Doctrine\ORM\EntityManager */
+use Slim\Slim;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Psr\Log\LogLevel;
+use Slim\Views\Twig;
+use Slim\Views\TwigExtension;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
 
-require '../bootstrap.php';
+require '../vendor/autoload.php';
 
-$config = array(
-    'templates.path' => '../templates/basic'
-);
+/**
+ * 
+ * @property Twig $view Twig
+ */
+class Application extends Slim
+{
 
-// Prepare app
-$app = new \Slim\Slim($config);
-$app->setName('TIKom');
+    /**
+     * Doctrine EntityManager
+     * @var EntityManager;
+     */
+    protected $em;
 
-// Create monolog logger and store logger in container as singleton 
-// (Singleton resources retrieve the same log resource definition each time)
-$app->container->singleton('log', function () {
-            $log = new \Monolog\Logger('slim-skeleton');
-            $log->pushHandler(new \Monolog\Handler\StreamHandler('../logs/app.log', \Psr\Log\LogLevel::DEBUG));
-            return $log;
-        });
+    public function __construct(array $userSettings = array())
+    {
+        parent::__construct(array(
+            'templates.path' => '../templates/basic',
+            'view' => 'Slim\Views\Twig'
+        ));
 
-// Prepare view
-$app->view(new \Slim\Views\Twig());
-$app->view->parserOptions = array(
-    'charset' => 'utf-8',
-    'cache' => realpath('../templates/cache'),
-    'auto_reload' => true,
-    'strict_variables' => false,
-    'autoescape' => true
-);
-$app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
+        $this->view->parserOptions = array(
+            'charset' => 'utf-8',
+            'cache' => realpath('../templates/cache'),
+            'auto_reload' => true,
+            'strict_variables' => false,
+            'auto_escape' => true
+        );
 
-$app->hook('slim.before.router', function() use ($app) {
-    $template = 'template.twig';
-    if($app->request()->headers('X-Pjax') === 'true')
-        $template = 'template_pjax.twig';
-    $app->view()->set('template', $template);
-});
+        $this->view->parserExtensions = array(new TwigExtension());
 
-$app->get('/', function() use ($app, $em) {
-            $app->log->info("Slim-Skeleton '/' route");
+        $this->setName('TikomLab');
 
-            $articleRepo = $em->getRepository('Article');
-            $articles = $articleRepo->findAll();
-            $app->render('index.twig', array(
-                'articles' => $articles
-            ));
-        })->name('homepage');
+        $isDevMode = true;
+        $config = Setup::createAnnotationMetadataConfiguration(array(realpath('../src')), $isDevMode);
 
-$app->get('/:slug', function($slug) use($app, $em) {
-            $repo = $em->getRepository('Article');
-            $article = $repo->findOneBy(array('slug' => $slug));
-            if(null === $article)
-                $app->notFound();
-            
-            $app->render('article.twig', array(
-                'article' => $article
-            ));
-        })->name('article');
+        $conn = array(
+            'driver' => 'pdo_mysql',
+            'host' => 'localhost',
+            'user' => 'root',
+            'password' => '',
+            'dbname' => 'Blog'
+        );
 
-// Run app
+        $this->em = EntityManager::create($conn, $config);
+
+        $this->hook('slim.before.router', array($this, 'beforeRouter'));
+        $this->hook('slim.after.router', array($this, 'afterRouter'));
+
+        $this->get('/', array($this, 'homepage'))->name('homepage');
+        $this->get('/:slug', array($this, 'article'))->name('article');
+        $this->get('/create', array($this, 'create'))->name('create');
+        $this->post('/create', array($this, 'store'));
+    }
+
+    public function homepage()
+    {
+        $this->log->info("Slim-Skeleton '/' route");
+
+        $articleRepo = $this->em->getRepository('Article');
+        $articles = $articleRepo->findAll();
+        $this->render('index.twig', array(
+            'articles' => $articles
+        ));
+    }
+
+    public function article($slug)
+    {
+        $repo = $this->em->getRepository('Article');
+        $article = $repo->findOneBy(array('slug' => $slug));
+        if (null === $article)
+            $this->pass();
+
+        $this->render('article.twig', array(
+            'article' => $article
+        ));
+    }
+
+    public function create()
+    {
+        $this->render('create.twig');
+    }
+
+    public function store()
+    {
+        $data = $this->request()->post();
+        $article = new Article();
+        $article->setTitle($data['title']);
+        $article->setContent($data['content']);
+        $article->setCreated(new DateTime('now'));
+        $article->setSlug($this->slugify($data['title']));
+
+        $this->em->persist($article);
+        $this->em->flush();
+
+        $this->redirect('/');
+    }
+
+    public function beforeRouter()
+    {
+        $template = 'template.twig';
+        if ($this->request()->headers('X-Pjax') === 'true')
+            $template = 'template_pjax.twig';
+        $this->view->set('template', $template);
+    }
+
+    public function afterRouter()
+    {
+        $this->response()->header('X-PJAX-URL', $this->request()->getResourceUri());
+    }
+
+    /**
+     * 
+     * @return \Monolog\Logger
+     */
+    private function log()
+    {
+        $log = new Logger($this->getName());
+        $log->pushHandler(new StreamHandler('../logs/app.log', LogLevel::DEBUG));
+        return $log;
+    }
+
+    private function slugify($text)
+    {
+        // replace non letter or digits by -
+        $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+
+        // trim
+        $text = trim($text, '-');
+
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+        // lowercase
+        $text = strtolower($text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
+        if (empty($text)) {
+            return 'n-a';
+        }
+
+        return $text;
+    }
+
+}
+$app = new Application;
 $app->run();
--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,12 @@
-$.pjax.defaults.timeout = 5000;
+$.pjax.defaults.timeout = 0;
 $(document).pjax("a", "#pjax-container");
+$(document).on('pjax:start', function(e) {
+    $("#ajax-loader").css("visibility", "visible");
+});
+
+$(document).on('pjax:end', function(e) {
+    $("#ajax-loader").css("visibility", "hidden");
+});
+$(document).on('submit', 'form[data-pjax]', function(e) {
+    $.pjax.submit(e, '#pjax-container');
+});
